65c14c4253ef4c691e12fab142f3fe350a269b72
nexryai <61890205+nexryai@users.noreply.github.com>
2021-02-19 18:06:41 +0900

    Update makemy-dist.sh

--- makemy-dist.sh
+++ makemy-dist.sh
@@ -5,7 +5,7 @@ set -e
 echo ==========
 echo makemydist
 echo ==========
-echo 設定ファイルのコピーを開始します。root権限を使用します。
+echo "設定ファイルのコピーを開始します。root権限を使用します。"
 sudo cp -r $HOME/.local/share/applications/ /etc/skel/.local/
 sudo cp -r $HOME/.config/autostart/ /etc/skel/.config/
 sudo cp -r $HOME/.config/dconf/ /etc/skel/.config/
@@ -13,11 +13,11 @@ sudo cp -r $HOME/.config/gtk-2.0/ /etc/skel/.config/
 sudo cp -r $HOME/.config/gtk-3.0/ /etc/skel/.config/
 sudo cp -r $HOME/.config/Thunar/ /etc/skel/.config/
 sudo cp -r $HOME/.config/xfce4/ /etc/skel/.config/
-echo システムをアップデートします
+echo "システムをアップデートします"
 sudo apt-get update && apt-get upgrade -y
-echo 不要な依存関係を削除します
+echo "不要な依存関係を削除します"
 sudo apt autoremove --purge
 sudo PinguyBuilder clean
-echo 準備は完了しました。
+echo "準備は完了しました。"
 sudo PinguyBuilder dist
 sudo PinguyBuilder dist iso mydist.iso
